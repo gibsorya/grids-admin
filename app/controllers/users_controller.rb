749636190ce9627d3eb0bce121101794892b9f3a
Ryan Gibson <gibsorya@gmail.com>
2022-01-12 11:41:16 -0600

    added ability to index users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def index
+    users = User.order("created_at DESC")
+    render json: users
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  scope '/api/version1' do
+    resources :users
+  end
 end
